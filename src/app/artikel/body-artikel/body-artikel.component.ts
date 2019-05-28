2a407d298cf97a717647578ae6394961da4d2ee6
Rakish F <rakishfrisky@gmail.com>
2019-05-28 23:52:22 +0700

    add image for artikel using API
    Iterasi 1 completed

--- src/app/artikel/body-artikel/body-artikel.component.ts
+++ src/app/artikel/body-artikel/body-artikel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ConfigService } from 'src/app/services/config.service';
 import { BeritaService } from 'src/app/services/berita.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-body-artikel',
@@ -13,8 +14,10 @@ export class BodyArtikelComponent implements OnInit {
   newsDetail = [];
   id: any;
   dataLocal : any;
+  backgroundImageStyle = [];
+  photo : any = "http://172.18.16.52:3056/news/image/5ced372dab67f67e86047f78%20Cover.jpg"
 
-  constructor(private httpClient: HttpClient, private configService: ConfigService, public berita: BeritaService) { }
+  constructor(private httpClient: HttpClient, private configService: ConfigService, public berita: BeritaService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.dataLocal = JSON.parse(localStorage.getItem('newsDetail'));
@@ -24,6 +27,15 @@ export class BodyArtikelComponent implements OnInit {
 
   }
 
+  formatURL(data){
+    return data.split(' ').join('%20');
+  }
+
+  formatImageSrc(data){
+    // console.log("final", this.configService.baseUrl + "store/image/" + this.formatURL(data))
+    return this.configService.baseUrl + "news/image/" + this.formatURL(data);
+  }
+
   getNewsDetail(id) {
     this.berita.getNewsDetail(id).subscribe(data => {
       this.newsDetail = data.news;
@@ -31,5 +43,11 @@ export class BodyArtikelComponent implements OnInit {
     });
   }  
 
+  imageUrl(data) {
+    let link =  this.formatImageSrc(data);
+    console.log("Cek Link : ", link);
+    return link
+    // return this.sanitizer.bypassSecurityTrustStyle('url(' + link + ')');
+  }  
 
 }
